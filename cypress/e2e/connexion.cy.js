b47f8c45c9977887c9c6132487eee8ff3fb01d2b
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-05-28 16:40:33 +0200

    added default cypress

---
+++ cypress/e2e/connexion.cy.js
@@ -0,0 +1,14 @@
+it('Connexion', () => {
+cy.visit('/ma-page')
+cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
+cy.contains('label', 'Password').should('be.visible').type('123456')
+cy.get('button').should('contains.text', 'Connexion')
+})
+
+it('Inscription',()=>{
+cy.visit('/ma-page')
+cy.get('button').should('contains.text', 'Inscription')
+cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
+cy.contains('label', 'Password').should('be.visible').type('123456')
+cy.get('button').should('contains.text', 'Inscrit toi')
+})
