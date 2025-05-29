589cf6f2ecc11ffd342613c6c5510f33db8e4c77
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-05-29 19:01:26 +0200

    Conforme au cahier de test

--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -1,56 +1,57 @@
 it('Connexion avec un utilisateur valide', () => {
-    cy.visit('/ma-page')
+    cy.visit(baseUrl)
     cy.contains('label', 'Email').should('be.visible').type(user)
     cy.contains('label', 'Password').should('be.visible').type(mdp)
     cy.get('button').should('contains.text', 'Connexion').click({force})
 })
 
 it('Connexion avec un mauvais utilisateur et un bon mdp', () => {
-    cy.visit('/ma-page')
+    cy.visit(baseUrl)
     cy.contains('label', 'Email').should('be.visible').type(fakeuser)
     cy.contains('label', 'Password').should('be.visible').type(mdp)
     cy.get('button').should('contains.text', 'Connexion').click({force})
 })
 
 it('Connexion avec un mauvais mdp mais bon utilisateur', () => {
-    cy.visit('/ma-page')
+    cy.visit(baseUrl)
     cy.contains('label', 'Email').should('be.visible').type(user)
     cy.contains('label', 'Password').should('be.visible').type(fakemdp)
     cy.get('button').should('contains.text', 'Connexion').click({force})
 })
 
 it('Inscription', ()=>{
-    cy.visit('/ma-page')
+    cy.visit(baseUrl)
     cy.get('button').should('contains.text', 'Inscription')
     cy.contains('label', 'Email').should('be.visible').type(inscription)
     cy.contains('label', 'Password').should('be.visible').type(mdp2)
     cy.get('button').should('contains.text', 'Inscrit toi').click({force})
 })
 
-it('Affichage de la composition', ()=>{
-    cy.get()
-
+it('Affichage de la composition des pizzas', ()=>{
+    cy.visit(baseUrl)
+    //ajouter une rechercher si besoin pour tomber sur une pizza
+    cy.contains('desc').should('exist')
 
 })
 
 it('Affichage du prix des pizzas', ()=>{
-    cy.contais()
+     cy.contains('price').should('exist')
 })
 
 it('Ajout au panier', ()=>{
-    cy.contais()
+    cy.contains()
 })
 
 
 it('Consultation du panier', ()=>{
-    cy.contais()
+    cy.contains()
 })
 
 
 it('Finalisation de la commande', ()=>{
-    cy.contais()
+    cy.contains()
 })
 
 it('Déconnexion', ()=>{
-    cy.contais()
+    cy.contains()
 })
