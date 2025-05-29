58fa06cff53a6110aefc94cb82ce5763d2ac9263
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-05-29 18:48:47 +0200

    variables

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,17 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    env : {
+      baseUrl : "",
+      user : "test@gmail.com",
+      fakeuser:"gmail@gmail.com",
+      inscription : "test2@gmail.com",
+      mdp : "Test1234",
+      mdp2: "Test12345",
+      fakemdp:"test789",
+    }
+
   },
 });
--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -1,18 +1,56 @@
-it('Connexion', () => {
-cy.visit('/ma-page')
-cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
-cy.contains('label', 'Password').should('be.visible').type('123456')
-cy.get('button').should('contains.text', 'Connexion').click({force})
+it('Connexion avec un utilisateur valide', () => {
+    cy.visit('/ma-page')
+    cy.contains('label', 'Email').should('be.visible').type(user)
+    cy.contains('label', 'Password').should('be.visible').type(mdp)
+    cy.get('button').should('contains.text', 'Connexion').click({force})
+})
+
+it('Connexion avec un mauvais utilisateur et un bon mdp', () => {
+    cy.visit('/ma-page')
+    cy.contains('label', 'Email').should('be.visible').type(fakeuser)
+    cy.contains('label', 'Password').should('be.visible').type(mdp)
+    cy.get('button').should('contains.text', 'Connexion').click({force})
+})
+
+it('Connexion avec un mauvais mdp mais bon utilisateur', () => {
+    cy.visit('/ma-page')
+    cy.contains('label', 'Email').should('be.visible').type(user)
+    cy.contains('label', 'Password').should('be.visible').type(fakemdp)
+    cy.get('button').should('contains.text', 'Connexion').click({force})
 })
 
 it('Inscription', ()=>{
-cy.visit('/ma-page')
-cy.get('button').should('contains.text', 'Inscription')
-cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
-cy.contains('label', 'Password').should('be.visible').type('123456')
-cy.get('button').should('contains.text', 'Inscrit toi').click({force})
+    cy.visit('/ma-page')
+    cy.get('button').should('contains.text', 'Inscription')
+    cy.contains('label', 'Email').should('be.visible').type(inscription)
+    cy.contains('label', 'Password').should('be.visible').type(mdp2)
+    cy.get('button').should('contains.text', 'Inscrit toi').click({force})
 })
 
-it('Des pizzas?', ()=>{
+it('Affichage de la composition', ()=>{
+    cy.get()
+
+
+})
+
+it('Affichage du prix des pizzas', ()=>{
+    cy.contais()
+})
+
+it('Ajout au panier', ()=>{
+    cy.contais()
+})
+
+
+it('Consultation du panier', ()=>{
+    cy.contais()
+})
+
+
+it('Finalisation de la commande', ()=>{
+    cy.contais()
+})
+
+it('Déconnexion', ()=>{
     cy.contais()
 })
