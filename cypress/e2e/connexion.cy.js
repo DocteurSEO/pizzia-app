7972a78db60165a9c64d90f2e4d682702c575a69
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-05-29 18:15:24 +0200

    Update connexion.cy.js

--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -2,7 +2,7 @@ it('Connexion', () => {
 cy.visit('/ma-page')
 cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
 cy.contains('label', 'Password').should('be.visible').type('123456')
-cy.get('button').should('contains.text', 'Connexion')
+cy.get('button').should('contains.text', 'Connexion').click({force})
 })
 
 it('Inscription', ()=>{
@@ -10,7 +10,9 @@ cy.visit('/ma-page')
 cy.get('button').should('contains.text', 'Inscription')
 cy.contains('label', 'Email').should('be.visible').type('test@gmail.com')
 cy.contains('label', 'Password').should('be.visible').type('123456')
-cy.get('button').should('contains.text', 'Inscrit toi')
+cy.get('button').should('contains.text', 'Inscrit toi').click({force})
 })
 
-
+it('Des pizzas?', ()=>{
+    cy.contais()
+})
