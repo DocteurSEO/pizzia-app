17a44fae56d135c0c4fb8f6d91c5791b8334528d
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-05-30 20:40:52 +0200

    Ajout du test inscription et vérifications des pizzas

--- cypress/e2e/inscription.cy.js
+++ cypress/e2e/inscription.cy.js
@@ -1,8 +1,42 @@
-it('Inscription', ()=>{
-    cy.visit("http://localhost:3000/login")
+import {UrlLogin} from "./../fixtures/inscription.json"
+import {email} from "./../fixtures/inscription.json"
+import {mdp} from "./../fixtures/inscription.json"
+import {lastName} from "./../fixtures/inscription.json"
+import {firstName} from "./../fixtures/inscription.json"
+import {validateFakeMdp} from "./../fixtures/inscription.json"
+import {validateMdp} from "./../fixtures/inscription.json"
+
+
+beforeEach(() =>{
+    cy.visit(UrlLogin)
+});
+
+it('Inscription avec de bonnes données', ()=>{
+
     cy.get('.page-module__X_Z_4a__btn_inscription').contains('Inscription').click({force:true})
     cy.wait(1000)
-    cy.get('input[placeholder="Adresse email"]').type("register.cypress@gmail.com")
-    cy.get('input[placeholder="Mot de passe"]').type("Test12345")
+    cy
+        .get('input[placeholder="Adresse email"]').type(email)
+        .get('input[placeholder="Nom"]').type(lastName)
+        .get('input[placeholder="Prénom"]').type(firstName)
+        .get('input[placeholder="Mot de passe"]').type(mdp)
+        .get('input[placeholder="Valider mot de passe"]').type(validateMdp)
+
+
+    cy.get('.page-module__IMkl-G__button_inscription').contains('INSCRIPTION').click({force:true})
+})
+
+it('Inscription avec un mauvais mot de passe en validation', ()=>{
+
+    cy.get('.page-module__X_Z_4a__btn_inscription').contains('Inscription').click({force:true})
+    cy.wait(1000)
+    cy
+        .get('input[placeholder="Adresse email"]').type(email)
+        .get('input[placeholder="Nom"]').type(lastName)
+        .get('input[placeholder="Prénom"]').type(firstName)
+        .get('input[placeholder="Mot de passe"]').type(mdp)
+        .get('input[placeholder="Valider mot de passe"]').type(validateFakeMdp)
+
+
     cy.get('.page-module__IMkl-G__button_inscription').contains('INSCRIPTION').click({force:true})
 })
--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -1,10 +1,22 @@
+import {UrlPizza} from "./../fixtures/pizza.json"
+
+beforeEach(() =>{
+    cy.visit(UrlPizza)
+});
+
+it('Affichage des pizzas', ()=>{
+ 
+    cy.get(".cardProduct-module__B2l_pG__productCard").contains('Pizza Hawaïenne').should('be.visible')
+
+})
+
+
 it('Affichage de la composition des pizzas', ()=>{
-    cy.visit("http://localhost:3000")
-    //ajouter une rechercher si besoin pour tomber sur une pizza
+ 
     cy.contains('desc').should('exist')
 
 })
 
 it('Affichage du prix des pizzas', ()=>{
-     cy.contains('price').should('exist')
+     cy.get(':nth-child(1) > :nth-child(3) > strong').should('have.text', '1 €')
 })
