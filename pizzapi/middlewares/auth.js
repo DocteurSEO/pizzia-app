73afedd0936b1b31361d60e0deae0b59db14c374
Oudart Matéo <mateooudart@gmail.com>
2025-06-01 22:55:30 +0200

    added: isAdmin & auth middlewares to handle permissions

--- pizzapi/middlewares/auth.js
+++ pizzapi/middlewares/auth.js
@@ -1,36 +1,25 @@
-const e = require('express');
 const { auth } = require('../config/firebase');
 
 const requireAuth = (req, res, next) => {
-    const sessionCookie = req.session?.firebaseToken;
+  const sessionCookie = req.session?.firebaseToken;
 
-    if (!sessionCookie) {
-        return res.status(401).json({
-            error: 'auth/unauthorized',
-            message: 'You need to be logged in.'
-        });
-    }
+  if (!sessionCookie) {
+    return res.status(401).json({
+      error: 'auth/unauthorized',
+      message: 'You need to be logged in.'
+    });
+  }
 
-    auth.verifySessionCookie(sessionCookie, true)
-        .then((data) => {
-            req.user = data;
-            console.log(`User authenticated: ${req.user.email} with role ${req.user.admin}`);
-            
-            if (req.user.admin) {
-                next();
-            } else {
-                return res.status(403).json({
-                    error: 'auth/forbidden',
-                    message: 'You don\'t have permission to perform this action.'
-                });
-            }
-        })
-        .catch(() => {
-            return res.status(401).json({
-                error: 'auth/invalid-session',
-                message: 'Token expired.'
-            });
-        });
+  auth.verifySessionCookie(sessionCookie, true)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      return res.status(401).json({
+        error: 'auth/invalid-session',
+        message: 'Token expired.'
+      });
+    });
 };
 
 module.exports = { requireAuth };
--- pizzapi/routers/products.js
+++ pizzapi/routers/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getProducts, createProduct, updateProduct, getProductById, deleteProduct } = require('../controllers/products.js');
 const { requireAuth } = require('../middlewares/auth.js');
+const { admin } = require('../middlewares/admin.js');
 const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', requireAuth, createProduct);
-router.put('/:id', requireAuth, updateProduct);
-router.delete('/:id', requireAuth, deleteProduct);
+router.post('/', requireAuth, admin, createProduct);
+router.put('/:id', requireAuth, admin, updateProduct);
+router.delete('/:id', requireAuth, admin, deleteProduct);
 
 module.exports = router;
