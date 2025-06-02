017af45c201321eff120998eae9a644671c132ba
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-06-02 21:03:50 +0200

    Ajout de la déconnexion

--- cypress/e2e/deconnexion.cy.js
+++ cypress/e2e/deconnexion.cy.js
@@ -1,3 +1,15 @@
+import {UrlLogin} from "./../fixtures/connexion.json"
+import {mdp} from "./../fixtures/connexion.json"
+import {email} from "./../fixtures/connexion.json"
+
 it('Déconnexion', ()=>{
-    cy.contains()
+    cy.visit(UrlLogin) 
+    cy.get('input[placeholder="Adresse email"]').type(email)
+    cy.get('input[placeholder="Mot de passe"]').type(mdp)
+    cy.get('.page-module__X_Z_4a__btn_connexion').contains('Connexion').click({force:true})
+    cy.wait(1000)
+    cy.get('.navbar-module__QXtwcq__btn_deconnexion').should('be.visible').click({force : true})
+    cy.wait(500)
+    cy.get('.page-module__X_Z_4a__form').contains('Connexion').should('be.visible')
 })
+
