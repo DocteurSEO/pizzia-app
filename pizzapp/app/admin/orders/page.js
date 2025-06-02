b29793b032e574910e455dc25f21d03d472f63cc
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2025-06-02 22:11:25 +0200

    form page admin & style lien navbar

--- pizzapp/app/admin/orders/page.js
+++ pizzapp/app/admin/orders/page.js
@@ -2,24 +2,33 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
-export default function ordersManager(){
+export default function ordersManager() {
 
-    const [activeForm, setActiveForm] = useState('');
+    const [activeForm, setActiveForm] = useState('update');
 
-    return(
+    return (
         <div className={styles.container_orders}>
             <div className={styles.container_dt_forms}>
                 <div className={styles.dt}></div>
                 <div className={styles.forms}>
                     <div className={styles.switchButtons}>
-                        <button onClick={() => setActiveForm('update')}></button>
-                        <button onClick={() => setActiveForm('delete')}></button>
+                        <button onClick={() => setActiveForm('update')}>Modifier</button>
+                        <button onClick={() => setActiveForm('delete')}>Supprimer</button>
                     </div>
 
-                    {activeForm === 'update' &&(
+                    {activeForm === 'update' && (
                         <form className={styles.formUpdate}>
                             <h2>Modifier une commande</h2>
                             <input type="text" placeholder='Id de la commande'></input>
+                            <button>Modifier</button>
+                        </form>
+                    )}
+
+                    {activeForm === 'delete' && (
+                        <form className={styles.formDelete}>
+                            <h2>supprimer une commande</h2>
+                            <input type="text" placeholder='Id de la commande'></input>
+                            <button>Supprimer</button>
                         </form>
                     )}
                 </div>
